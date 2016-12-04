44ada5367844a7e8e79e9c37572f7eb49e738a12
jonas <jonas>
2016-12-04 21:47:05 +0100

    resourceTileInit.txt

--- src/settlers/graphics/maps/Map.java
+++ src/settlers/graphics/maps/Map.java
@@ -1,4 +1,11 @@
 package settlers.graphics.maps;
+import java.util.List;
+
+import settlers.graphics.ResourceTile;
+//import settlers.graphics.NumberTile;
+//import settlers.graphics.PortTile;
+
+
 /**
  Creates a Map object which is used to create different maps from:
  - resource tiles
@@ -7,15 +14,14 @@ package settlers.graphics.maps;
  OBS! Only map specific graphical objects are created here! 
  * */
 
- 
 
 public class Map {
 	private int x; // x-position of map (left top corner) 
 	private int y; // x-position of map (left top corner) 
-	private List<resourceTile> resourceTiles;
-	private List<numberTile> numberTiles;
-	private List<portTile> portTiles;
-	private List<List> mapList; 
-	public Map(int x, int y)
+	private List<ResourceTile> resourceTiles;
+//	private List<NumberTile> numberTiles;
+//	private List<PortTile> portTiles;
+//	private List<List> mapList; 
+//	public Map(int x, int y)
 	
 }
