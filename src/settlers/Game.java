54688563306249284263a25318bf8171b559b5c6
albu9304 <alexander.bukk@gmail.com>
2016-12-11 19:19:34 +0100

    Trying to make many windows

--- src/settlers/Game.java
+++ src/settlers/Game.java
@@ -2,6 +2,9 @@ package settlers;
 
 import java.awt.Graphics;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.swing.JPanel;
 
 import settlers.graphics.DragAndDropListener;
@@ -12,16 +15,23 @@ public class Game extends JPanel {
 	// Member variables
 	private static final long serialVersionUID = 1L;
 	private Board board;
+	private List<Player> players = new ArrayList<Player>();
 	
-	public Game() throws IOException {
+	public Game(int numberOfPlayers) throws IOException {
 		// Create map (game board)
 		board = new Board("water", 0, 0);
-		
+		addPlayers(numberOfPlayers);
 		// add mouse listeners to enable drag and drop
 		//
 		DragAndDropListener listener = new DragAndDropListener(this.board.getResourceTiles(), this);
 		this.addMouseListener(listener);
 		this.addMouseMotionListener(listener);
+	}
+	
+	private void addPlayers(int numberOfPlayers) {
+		for (int i = 0; i < numberOfPlayers; i++) {
+			this.players.add(new Player(i));
+		}
 	}
 	
 	public Board getBoard() {
